aa3ae1e9cb15c8772eb82a2418388d7f6e71ca29
DansuraitoAhiru <vqviet2007@gmail.com>
2026-07-14 22:38:50 +0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,38 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from typing import Literal
+
+bookings = Table(
+    "bookings",
+    Base.metadata,
+    Column("driver_id", Integer, ForeignKey("drivers.id"), primary_key=True),
+    Column("car_id", Integer, ForeignKey("cars.id"), primary_key=True)
+)
+
+
+class Fleet(Base):
+    __tablename__ = "fleets"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), nullable=False) 
+    driver = relationship("Driver", back_populates="fleet")
+
+
+class Driver(Base):
+    __tablename__ = "drivers"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    full_name = Column(String(100), nullable=False) 
+    status = Column(String(20), default="ACTIVE", nullable=False)
+    fleet_id = Column(Integer, ForeignKey("fleets.id"))
+    fleet = relationship("Fleet", back_populates="driver")
+    booking = relationship("Booking", back_populates="driver")
+    cars = relationship("Car", secondary="bookings", back_populates="drivers")
+
+
+class Car(Base):
+    __tablename__ = "cars"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    license_plate  = Column(String(20), nullable=False) 
+    status = Column(String(20), default="AVAILABLE", nullable=False)
+    booking = relationship("Booking", back_populates="car")
+    drivers = relationship("Driver", secondary="bookings", back_populates="cars")
